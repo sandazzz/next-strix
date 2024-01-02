d4837ff1e54f93086b3e046cdf8e3dad0527ecc8
sandazzz <rsandarisoa@gmail.com>
2024-01-02 15:24:44 +0700

    Update

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -52,7 +52,37 @@ export default function Cart() {
         <button className="btn btn-info" type="submit" role="link">
           Checkout
         </button>
-  </form>*/}
+  </form>*/
+  /*import { NextApiRequest, NextApiResponse } from "next";
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+export async function POST(req: NextApiRequest, res:NextApiResponse) {
+  if (req.method === 'POST') {
+    try {
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price: 'price_1OTTSMD72PzL52Rm2AFAsRd7',
+            quantity: 1,
+          },
+        ],
+        mode: 'payment',
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      });
+      res.redirect(303, session.url);
+    } catch (err: any) {
+      console.error(err);
+      res.status(500).json({ error: err.message });
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
+}
+*/}
       <section className="flex flex-col items-center">
         {cartItems.map((item, index) => (
           <div
