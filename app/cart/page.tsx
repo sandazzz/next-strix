5328e037585cae22143c7307cf7c9cc14a51bea9
sandazzz <rsandarisoa@gmail.com>
2024-01-02 14:52:08 +0700

    Update page.tsx

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import { loadStripe } from "@stripe/stripe-js";
+//import { loadStripe } from "@stripe/stripe-js";
 
 /*const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
