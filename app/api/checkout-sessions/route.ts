104cb69135e762d84eaa7211385f3106e762b08e
sandazzz <rsandarisoa@gmail.com>
2024-01-15 23:47:36 +0100

    Update route.ts

--- app/api/checkout-sessions/route.ts
+++ app/api/checkout-sessions/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-
+//const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")("sk_test_51OACd6D72PzL52RmteorfV4aTi9ndYv6T4JOJr5XKa0q4Rf5r3XF1OvzLBVUoBjomR2gLkqj4fuEHuUQnrDyhmwI00iJC2ugOv");
 export async function POST(request: Request, res: any) {
   const requestBody = await request.json();
   //console.log(requestBody);
