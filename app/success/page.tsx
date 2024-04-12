38be49ecf1b45d55146a8f0f39cfbfcaf32c10de
sandazzz <rsandarisoa@gmail.com>
2024-04-12 16:41:57 +0200

    Cancel page delete

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 
-export default function SuccessPage() {
+export default async function SuccessPage() {
   return (
     <div className='bg-gradient-to-r from-violet-500 to-fuchsia-500 min-h-screen p-7 md:px-16 text-white flex flex-col justify-center items-center'>
       <h1 className='text-5xl text-center	'>Order Successful!</h1>
