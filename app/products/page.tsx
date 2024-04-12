76be0c8a4fb1a78431565f861c3a6fbc040aa553
sandazzz <rsandarisoa@gmail.com>
2024-04-12 16:00:44 +0200

    Productpage Update

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,60 +1,17 @@
-//"use client";
 import { fetchProducts } from "@/app/lib/data";
-import Image from "next/image";
-import Link from "next/link";
-//import { useState } from "react";
+import ProductSection from "@/app/ui/products/ProductSection";
+import { useEffect } from "react";
 
 export default async function Products() {
-  //const [name, setName] = useState("");
-
   const products = await fetchProducts();
 
-  //const handleChange = (event) => {};
   return (
     <main>
-      
-        {/*<div id="search-bar" className="h-16 flex justify-center items-center">
-        <input
-          type="text"
-          placeholder="Filter by game"
-          className="input input-bordered input-primary w-full max-w-xs"
-          //onChange={handleChange()}
-        /> </div>*/}
-     
       <section
         id="product-container"
-        className="flex flex-col items-center md:flex-row md:justify-center md:gap-7 md:flex-wrap p-7 m-auto bg-gradient-to-tr from-blue-900 via-indigo-900 to-purple-900"
+        className="w-full min-h-screen flex flex-col items-center md:flex-row md:justify-center md:gap-7 md:flex-wrap p-7 m-auto bg-gradient-to-tr from-blue-900 via-indigo-900 to-purple-900"
       >
-        {products.map((p) => (
-          <div
-            key={p.id}
-            className="flex items-center gap-4
-              w-[370px] h-[300px] 
-              mb-3 border-solid border-2 border-purple-400 rounded-lg p-4
-              bg-opacity-10 bg-white backdrop-filter backdrop-blur-md"
-          >
-            <Image
-              width={181}
-              height={267}
-              src={p.image}
-              alt={p.name}
-              className="rounded-md"
-            />
-            <div className="flex flex-col gap-2 text-white">
-              <h2 className="text-2xl">{p.name}</h2>
-              <h3 className="text-xl">{p.game}</h3>
-              <p>{p.description}</p>
-              <p>Starting from {p.lite_plan}€/month</p>
-              <Link
-                className="btn btn-outline btn-info"
-                href={`/products/${p.id}`}
-                passHref
-              >
-                Voir les détails
-              </Link>
-            </div>
-          </div>
-        ))}
+        <ProductSection products={products} />
       </section>
     </main>
   );
---
+++ app/ui/products/ProductSection.tsx
@@ -0,0 +1,64 @@
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Product } from "@/app/lib/definition";
+
+export default function ProductSection({ products }: { products: any }) {
+  const [filtredProduct, setFiltredProduct] = useState(products);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value === "") {
+      setFiltredProduct(products);
+      return;
+    } else {
+      const inputValue = event.target.value.toLowerCase();
+      const newProduct = products.filter((product: any) =>
+        product.game.toLowerCase().includes(inputValue)
+      );
+      setFiltredProduct(newProduct);
+    }
+  };
+  return (
+    <>
+      <div id="search-bar" className="h-16 w-[370px] flex justify-center items-center">
+        <input
+          type="text"
+          placeholder="Filter by game"
+          className="input input-bordered input-primary w-full max-w-xs"
+          onChange={handleChange}
+        />
+      </div>
+      {filtredProduct.map((p: Product) => (
+        <div
+          key={p.id}
+          className="flex items-center gap-4
+              w-[370px] h-[300px] 
+              mb-3 border-solid border-2 border-purple-400 rounded-lg p-4
+              bg-opacity-10 bg-white backdrop-filter backdrop-blur-md"
+        >
+          <Image
+            width={181}
+            height={267}
+            src={p.image}
+            alt={p.name}
+            className="rounded-md"
+          />
+          <div className="flex flex-col gap-2 text-white">
+            <h2 className="text-2xl">{p.name}</h2>
+            <h3 className="text-xl">{p.game}</h3>
+            <p>{p.description}</p>
+            <p>Starting from {p.lite_plan}€/month</p>
+            <Link
+              className="btn btn-outline btn-info"
+              href={`/products/${p.id}`}
+              passHref
+            >
+              Voir les détails
+            </Link>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
