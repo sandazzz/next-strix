718c11a3ce8f0edeea436881191e36b781c172f8
sandazzz <rsandarisoa@gmail.com>
2024-07-22 15:47:28 +0700

    homepage changes

---
+++ app/(home)/page.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import GridOfGame from "../ui/main/GridOfGame";
+import { fetchTwoProducts } from "@/app/lib/data";
+import MainPageProduct from "../ui/main/MainPageProduct";
+
+export default async function Home() {
+  const products = await fetchTwoProducts();
+  return (
+    <main className=" bg-white min-h-screen w-full flex flex-col justify-evenly items-center">
+      <section
+        id="main-title"
+        className="flex flex-col justify-center gap-4 p-6"
+      >
+        <h1 className="font-medium text-2xl md:text-3xl text-[#9733b6]">
+          <Link href="#products-container">
+            Learn new techs, improve your skills and hit your dreamed rank
+          </Link>
+        </h1>
+        <h2 className="text-center font-semibold text-2xl ">
+          Search by game, mentor or company
+        </h2>
+      </section>
+
+      <GridOfGame />
+
+      <section
+        id="product-container"
+        className="w-full h-min p-7 flex flex-col items-center md:flex-row md:justify-center gap-7 bg-gradient-to-tr from-blue-900 via-indigo-900 to-purple-900"
+      >
+        {products.map((p) => (
+          <MainPageProduct key={p.id} product={p}></MainPageProduct>
+        ))}
+      </section>
+    </main>
+  );
+}
---
+++ app/ui/main/MainPageProduct.tsx
@@ -0,0 +1,33 @@
+import { Product } from "@/app/lib/definition";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function MainPageProduct({ product }: {product : Product}) {
+  return (
+    <div className="flex flex-col md:flex-row justify-center items-center w-5/6 max-w-xl h-auto md:h-96 m-2">
+      <Image
+        width={256}
+        height={384}
+        alt="coach-image"
+        className="object-cover w-full md:w-64 h-64 md:h-96 border-2 border-indigo-200 rounded-t-xl md:rounded-3xl"
+        src={product.image}
+      />
+      <div className="bg-purple-700 bg-opacity-80 backdrop-blur-sm w-full md:w-60 h-56 md:h-[90%] border-2 border-t-0 md:border-t-2 md:border-l-0 md:border-indigo-100 border-opacity-10 rounded-b-xl md:rounded-r-xl md:rounded-b-none relative flex flex-col items-start p-5 text-white duration-500 hover:bg-indigo-900 hover:border-indigo-400">
+        <h2 className="mb-2 text-xl font-bold">{product.name}</h2>
+        <h3 className="mb-2 text-lg text-indigo-100">{product.game}</h3>
+        <p className="mb-2">{product.description}</p>
+        <p className="mb-2 text-yellow-300 font-bold">
+          Starting from
+          <span className="text-2xl">{product.lite_plan}€</span>/month
+        </p>
+        <Link
+          className="bg-blue-600 rounded-full py-1 px-2 text-lg text-white cursor-pointer absolute bottom-5 right-5 transition duration-500 hover:bg-blue-400 hover:shadow-lg"
+          type="button"
+          href={`/products/${product.id}`}
+        >
+          View More
+        </Link>
+      </div>
+    </div>
+  );
+}
